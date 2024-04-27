cc092885b261535aa928b934db64c3a2c8d1c30c
MinhNguyenTang <56515817+MinhNguyenTang@users.noreply.github.com>
2024-04-27 18:40:08 +0200

    Trying to fix issues

--- my-symfony-project/config/bundles.php
+++ my-symfony-project/config/bundles.php
@@ -16,4 +16,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
