ad3fe3ec2166cb388e494a5d2e9b106fd91d337d
MinhNguyenTang <56515817+MinhNguyenTang@users.noreply.github.com>
2024-05-27 12:32:13 +0200

    User Password Edition

--- my-symfony-project/src/Controller/UserController.php
+++ my-symfony-project/src/Controller/UserController.php
@@ -4,11 +4,13 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserType;
+use App\Form\UserPasswordType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserController extends AbstractController
 {
@@ -48,6 +50,55 @@ class UserController extends AbstractController
         }
 
         return $this->render('pages/user/edit_profile.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('user/edition-password/{id}', name: 'app_user_edit-pwd', methods: ['GET', 'POST'])]
+    public function editPassword(
+        User $user,
+        Request $request,
+        EntityManagerInterface $manager,
+        UserPasswordHasherInterface $passwordHasher,
+    ): Response
+    {
+        if(!$this->getUser()) 
+        {
+            return $this->redirectToRoute('app_login');
+        } 
+        
+        if($this->getUser() !== $user) 
+        {
+            return $this->redirectToRoute('app_home');
+        }
+
+        $form = $this->createForm(UserPasswordType::class, $user);
+
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $newPwd = $user->getPassword();
+            $newHashed = $passwordHasher->hashPassword(
+                $user,
+                $newPwd
+            );
+
+            $user = $form->getData();
+            $user->setPassword($newHashed);
+
+            $this->addFlash(
+                'success',
+                'Your password has been successfully modified!'
+            );
+
+            $manager->persist($user);
+            $manager->flush();
+
+            return $this->redirectToRoute('app_ingredient');
+
+        }
+        return $this->render('pages/user/edit_password.html.twig', [
             'form' => $form->createView()
         ]);
     }
--- my-symfony-project/src/Form/UserPasswordType.php
+++ my-symfony-project/src/Form/UserPasswordType.php
@@ -5,14 +5,42 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class UserPasswordType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('password', PasswordType::class, [
-                
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'attr' => [
+                        'class' => 'form-control'
+                    ],
+                    'label' => 'New password',
+                    'label_attr' => [
+                        'class' => 'form-label mt-4'
+                    ],
+                ],
+                'second_options' => [
+                    'attr' => [
+                        'class' => 'form-control'
+                    ],
+                    'label' => 'Confirm password',
+                    'label_attr' => [
+                        'class' => 'form-label mt-4'
+                    ],
+                ],
+                'invalid_message' => 'The password fields must match.',
+                'required' => true,
+                'constraints' => [
+                    New Assert\NotBlank()
+                ]
             ])
             ->add('submit', SubmitType::class, [
                 'attr' => [
