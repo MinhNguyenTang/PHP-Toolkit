0245194a5ba6dbd0cccaf937949d5c14f5c05393
MinhNguyenTang <56515817+MinhNguyenTang@users.noreply.github.com>
2024-05-27 09:51:06 +0200

    Laravel Installation

---
+++ my-symfony-project/src/Form/UserPasswordType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserPasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('password', PasswordType::class, [
+                
+            ])
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-primary mt-4'
+                ],
+                'label' => 'Submit'
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'csrf_protection' => true
+        ]);
+    }
+}
