6789186868e51809cdd3f00c4042f0a87c37599e
MinhNguyenTang <56515817+MinhNguyenTang@users.noreply.github.com>
2024-06-28 18:12:53 +0200

    Pattern updated

--- my-symfony-project/src/Entity/User.php
+++ my-symfony-project/src/Entity/User.php
@@ -53,7 +53,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     #[Assert\NotBlank()]
     #[Assert\Regex(
-        pattern: "/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&amp;*_=+\-]).{10,255}$/",
+        pattern: "/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&;*_=+\-]).{10,65}$/",
         message: "The password must contain at least one lowercase letter, one uppercase letter, one number, one special character and be at least 10 characters long."
     )]
     private ?string $password = null;
